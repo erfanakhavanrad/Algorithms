4e89440487c23e4dc977bd0f95cc7bd97e32be56
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2024-01-26 20:45:57 +0330

    Dijkstra Algorithm

--- src/main/java/inde/Dijkstra/Dijkstra.java
+++ src/main/java/inde/Dijkstra/Dijkstra.java
@@ -1,7 +1,8 @@
 package inde.Dijkstra;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Erfan Akhavan Rad
@@ -9,10 +10,42 @@ import java.util.Set;
  */
 public class Dijkstra<T> {
 
-    public void calculateShortestPath(Node<T> source){
+    public void calculateShortestPath(Node<T> source) {
         source.setDistance(0);
         Set<Node<T>> settledNodes = new HashSet<>();
+        Queue<Node<T>> unsettledNodes = new PriorityQueue<>(Collections.singleton(source));
+        while (!unsettledNodes.isEmpty()) {
+            Node<T> currentNode = unsettledNodes.poll();
+            currentNode.getAdjacentNodes()
+                    .entrySet().stream()
+                    .filter(entry -> !settledNodes.contains(entry.getKey()))
+                    .forEach(entry -> {
+                        evaluateDistanceAndPath(entry.getKey(), entry.getValue(), currentNode);
+                        unsettledNodes.add(entry.getKey());
+                    });
+            settledNodes.add(currentNode);
+        }
 
+    }
+
+    private void evaluateDistanceAndPath(Node<T> adjacentNode, Integer edgeWeight, Node<T> sourceNode) {
+        Integer newDistance = sourceNode.getDistance() + edgeWeight;
+        if (newDistance < adjacentNode.getDistance()) {
+            adjacentNode.setDistance(newDistance);
+            adjacentNode.setShortestPath(Stream.concat(sourceNode.getShortestPath().stream(), Stream.of(sourceNode)).toList());
+        }
+    }
+
+    public void printPaths(List<Node<T>> nodes) {
+        nodes.forEach(node -> {
+            String path = node.getShortestPath().stream()
+                    .map(Node::getName).map(Objects::toString)
+                    .collect(Collectors.joining(" -> "));
+            System.out.println((path.isBlank()
+                    ? "%s : %s".formatted(node.getName(), node.getDistance())
+                    : "%s -> %s : %s".formatted(path, node.getName(), node.getDistance())
+            ));
+        });
     }
 
 }
